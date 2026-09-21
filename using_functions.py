9884ece58cba5bf23477cef017b6153d238766c7
onionmccabbage <onionmccabbage@live.com>
2026-09-21 12:00:35 +0100

    combining functions, logic and loops

--- using_functions.py
+++ using_functions.py
@@ -4,14 +4,15 @@ def checkType(v):
     '''We may choose to write a docstring explaining our function
     In this case, we will check the type of a variable'''
     if type(v) == int:
-        # f'' lets us format the string
-        return f"{v} is an integer"
+        return (v, 'int')
     elif type(v) == float:
-        return f"{v} is a float"   
+        return (v, 'float')   
     elif type(v) == str:
-        return f"{v} is a string"  
+        return (v, 'string')  
 
 # we may exercise the code
-d = 4
-result = checkType(d)
-print(result)
+data = [4,5.1, 9, -3, 44.444, 'lunch']
+for d in data:
+    result = checkType(d)
+    # f'' lets us format the string
+    print(f"{result[0]} is a {result[1]}")
