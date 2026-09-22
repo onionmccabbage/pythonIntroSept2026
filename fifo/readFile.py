a6c46e79a21b3399ce8ef24817be5d3cd7e4dd43
onionmccabbage <onionmccabbage@live.com>
2026-09-22 16:28:15 +0100

    using try...except with file read and write

--- fifo/readFile.py
+++ fifo/readFile.py
@@ -2,15 +2,27 @@
 
 def readWith():
     '''retrieve and return the entire contents of a text file'''
-    with open('my_log.txt', 'rt') as fin: # 'rt' will read text
-        r = fin.read()
-        return r
+    try:
+        with open('my_log.txt', 'rt') as fin: # 'rt' will read text
+            r = fin.read()
+            return r
+    except FileNotFoundError as fnf:
+        print(f'Cannot locate file: {fnf}')
+    except Exception as err: # we always handle generic exceptions after specific exceptions
+        print(err)
+    finally:
+        print('This optional block will always run')
 
 def readAsList():
     '''retrieve the entire contents of a file as a list of text lines'''
-    with open('my_log.txt', 'rt') as fin:
-        r_l = fin.readlines() # retrieve each line as a string in a list
-        return r_l
+    try:
+        with open('my_log.txt', 'rt') as fin:
+            r_l = fin.readlines() # retrieve each line as a string in a list
+            return r_l
+    except FileNotFoundError as fnf:
+        print(f'Cannot locate file: {fnf}')
+    except Exception as err:
+        print(err)
 
 if __name__ == '__main__':
     print( readWith() )
--- fifo/writeFile.py
+++ fifo/writeFile.py
@@ -2,6 +2,7 @@
 
 def writeToFile(b):
     '''Persist the entire contents of b into a text file'''
+    # NB good idea to use try...except here
     fout = open('my_log.txt', 'a') # the default is 't' for text
     fout.write(b)
     fout.close()
@@ -9,10 +10,21 @@ def writeToFile(b):
 # there is an alternative syntax which is more performant
 def writeWith(n):
     '''use the 'with' operator to persist in a text file'''
-    with( open('log.txt', 'a') as fout ):
-        fout.write(n)
-        fout.write('\n')
-    # the 'with' operator will close the asset as soon as no longer used
+    fout=False
+    try:
+        with( open('log.txt', 'a') as fout ):
+            fout.write(n)
+            fout.write('\n')
+        # the 'with' operator will close the asset as soon as no longer used
+    except FileExistsError as fee:
+        print(f'Cannot use an existing file {fee}')
+    except FileNotFoundError as fnf:
+        print(f'Cannot locate file {fnf}')
+    except Exception as err:
+        print(err)
+    finally:
+        if fout:
+            fout.close() # if there is still a file access object, we close it
 
 if __name__ == '__main__':
     l = ['report', 'analysis', 'outcome', 'debrief']
